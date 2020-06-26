7292f88153549da2b8b486738db16637ac92aec3
Wilson <makoaewilson@gmail.com>
2020-06-26 05:46:57 +0200

    added function to comapre two strings

---
+++ Day05/ex03/ft_strcmp.c
@@ -0,0 +1,11 @@
+int ft_strcmp(char *s1, char *s2, unsigned int n)
+{
+	unsigned int i;
+
+	i = 0;
+	if (n == 0)
+		return (0);
+	while (s1[i] == s2[i] && s1[i] != '\0' && s2[i] != '\0'){
+		i++;}
+	return (s1[i] - s2[i]);
+}
